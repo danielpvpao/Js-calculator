c7f5adc03a5550e97b21909f6931d03b42439dcb
Daniel <96553464+danielpvpao@users.noreply.github.com>
2024-04-23 17:40:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+let OldNumber =  '';
+let Newnumber = '';
+let Operator ='';
+let display = document.getElementById('display');
+let result = '';
+display.textContent = '0';
+function OperatorPlus(){
+    Operator = '+'
+    Calc()
+    Updatedisplay();
+};
+function OperatorMinus(){
+    Operator = '-'
+    Calc()
+    Updatedisplay();
+};
+function OperatorMultiplication(){
+    Operator = 'X'
+    Calc()
+    Updatedisplay();
+};
+function OperatorDivision(){
+    Operator = '/'
+    Calc()
+    Updatedisplay();
+};
+function OperatorRoot(){
+    Operator ='√'
+    Calc()
+    Updatedisplay()
+};
+function setNumber(number) {
+    if(Operator == ''){
+    OldNumber += number
+ }  if(Operator !== ''){
+    Newnumber += number   
+     }
+    Updatedisplay();
+    Calc(OldNumber,Newnumber);
+};
+
+function Calc(){
+    if(OldNumber && Operator && Newnumber !== ''){
+    switch (Operator)
+    {
+        case '+': result = parseInt(OldNumber) + parseInt(Newnumber);
+        break;
+        case '-': result = parseInt(OldNumber -  Newnumber);
+        break;
+        case 'X': result = parseInt(OldNumber * Newnumber);
+        break;
+        case '/': result = parseInt(OldNumber / Newnumber);
+        break;
+        case '√': result = Math.pow(OldNumber, 1 / Newnumber);
+            result = parseFloat(result.toFixed(2));
+        break;
+    }
+} 
+
+}
+     
+function Updatedisplay() {
+ if(OldNumber == ''){
+    display.textContent = ' ' + ' ' + ' '
+ }
+ if(OldNumber !== ''){
+    display.textContent = OldNumber + ' ' + ' ';
+ }if(OldNumber && Operator !== ''){
+    display.textContent = OldNumber + Operator + ' ';
+ }
+ if(OldNumber && Operator && Newnumber !== ''){
+    display.textContent = OldNumber + Operator + Newnumber;
+ }
+};
+ 
+function displayResult() {
+    display.textContent = result;
+    Newresult()
+};
+function Clear(){
+    OldNumber = ''
+    Operator = ''
+    Newnumber = ''
+    result = ''
+    Updatedisplay()
+}
+function Newresult(){
+    OldNumber = result
+    Operator = ''
+    Newnumber = ''
+}
+
